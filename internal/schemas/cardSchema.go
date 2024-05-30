481b1218b3181ea9b97543b3f851f46eaa402fa6
DIMA KARPENKO <dimaskarpas@gmail.com>
2024-05-30 20:29:42 +0300

    refactor(schema): add UpdateCardReq and UpdateCardResp

--- internal/schemas/cardSchema.go
+++ internal/schemas/cardSchema.go
@@ -11,3 +11,16 @@ type CreateCardResp struct {
 	Answer       string `json:"answer"`
 	CollectionID int    `json:"collection_id"`
 }
+
+type UpdateCardReq struct {
+	ID       int    `json:"id" validate:"required"`
+	Question string `json:"question,omitempty"`
+	Answer   string `json:"answer,omitempty"`
+}
+
+type UpdateCardResp struct {
+	ID           int    `json:"id"`
+	Question     string `json:"question"`
+	Answer       string `json:"answer"`
+	CollectionID int    `json:"collection_id"`
+}
