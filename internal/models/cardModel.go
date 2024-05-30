d21103727f73a519f2b86a5b49bc723326928004
Dmitrii Karpenko <dimaskarpas@gmail.com>
2024-05-30 22:47:56 +0300

    refactor(models): updated the Answers field to optional

--- internal/models/cardModel.go
+++ internal/models/cardModel.go
@@ -4,5 +4,5 @@ type Card struct {
 	ID           int    `gorm:"primary_key;autoIncrement"`
 	CollectionID int    `gorm:"not null"`
 	Question     string `gorm:"not null"`
-	Answer       string `gorm:"not null"`
+	Answer       string
 }
