69d16e591fe1f5cea8c8adf38e71993e98bc218a
Dmitrii Karpenko <dimaskarpas@gmail.com>
2024-05-31 22:45:10 +0300

    fix: delete secret word

--- internal/models/userModel.go
+++ internal/models/userModel.go
@@ -6,7 +6,6 @@ type User struct {
 	ID           int       `gorm:"primaryKey;autoIncrement"`
 	Username     string    `gorm:"unique;not null"`
 	PasswordHash string    `gorm:"not null"`
-	SecretWord   string    `gorm:"not null"`
 	CreatedAt    time.Time `gorm:"autoCreateTime"`
 
 	Collections []Collection
