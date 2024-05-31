f605b9b274ac365ef9b77a47194c0ab2802e2db5
Dmitriy Lapin <dima.ladmol@yandex.ru>
2024-06-01 00:29:05 +0300

    feat: add test signup

---
+++ tests/createUser_test.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"back/config"
+	"back/db"
+	"back/internal/handler"
+	"back/internal/models"
+	"back/internal/repository"
+	"back/internal/service"
+	"back/internal/util"
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestUserSignup(t *testing.T) {
+	cfg := config.NewConfig()
+
+	database, err := db.NewDatabase(cfg.Database.DSN())
+	if err != nil {
+		t.Fatalf("could not initialize database connection: %s", err)
+	}
+
+	// Инициализация компонентов
+	validator := util.NewValidator()
+	repos := repository.NewRepository(database.GetDB())
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services, validator)
+	r := handlers.InitRoutes()
+
+	// Создание тестового сервера
+	ts := httptest.NewServer(r)
+	defer ts.Close()
+
+	hashedPassword, err := util.HashPassword("testpassword")
+	if err != nil {
+		t.Fatalf("could not hash password: %s", err)
+	}
+
+	// Создание пользователя для теста
+	testUser := models.User{
+		Username:     "testuser",
+		PasswordHash: hashedPassword,
+		CreatedAt:    time.Now(),
+	}
+
+	// Подготовка данных для запроса
+	signupData := map[string]string{
+		"username": "testuser",
+		"password": "testpassword",
+	}
+	jsonData, err := json.Marshal(signupData)
+	if err != nil {
+		t.Fatalf("could not marshal signup data: %s", err)
+	}
+
+	// Выполнение запроса на эндпоинт /auth/signup
+	resp, err := http.Post(ts.URL+"/auth/signup", "application/json", bytes.NewBuffer(jsonData))
+	if err != nil {
+		t.Fatalf("could not send signup request: %s", err)
+	}
+	defer resp.Body.Close()
+
+	// Проверка статуса ответа
+	assert.Equal(t, http.StatusCreated, resp.StatusCode)
+
+	// Проверка созданного пользователя в базе данных
+	createdUser, err := repos.GetUserByUsername("testuser")
+	if err != nil {
+		t.Fatalf("could not get user by username: %s", err)
+	}
+
+	assert.Equal(t, testUser.Username, createdUser.Username)
+	assert.NoError(t, util.CheckPassword("testpassword", createdUser.PasswordHash))
+}
