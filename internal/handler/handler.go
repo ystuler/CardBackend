5263f2bfe5b489f4ec989ae84a30862e48516b44
Dmitriy Lapin <dima.ladmol@yandex.ru>
2024-05-31 17:38:37 +0300

    fix: better enpoints location in code

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -39,14 +39,15 @@ func (h *Handler) InitRoutes() *chi.Mux {
 		r.Post("/signup", h.signUp)
 		r.Post("/login", h.signIn)
 	})
-	r.Route("/profile", func(r chi.Router) {
-		r.Use(middleware.UserIdentity)
-		r.Get("/", h.getProfile)
-		r.Put("/username", h.updateUsername)
-		r.Put("/password", h.updatePassword)
-	})
+
 	r.Group(func(r chi.Router) {
 		r.Use(middleware.UserIdentity)
+
+		r.Route("/profile", func(r chi.Router) {
+			r.Get("/", h.getProfile)
+			r.Put("/username", h.updateUsername)
+			r.Put("/password", h.updatePassword)
+		})
 
 		r.Route("/collections", func(r chi.Router) {
 			r.Get("/", h.getAllCollections)
