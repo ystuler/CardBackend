22ba0acaac9fd2bf879226f4fd316b1bd7d99141
Dmitriy Lapin <dima.ladmol@yandex.ru>
2024-06-01 09:23:58 +0300

    refactor: remove unused error handling

--- internal/service/userService.go
+++ internal/service/userService.go
@@ -6,7 +6,6 @@ import (
 	"back/internal/schemas"
 	"back/internal/util"
 	"errors"
-	"gorm.io/gorm"
 	"time"
 )
 
@@ -20,9 +19,6 @@ func NewAuthService(repo repository.UserRepository) *AuthenticationImpl {
 
 func (s *AuthenticationImpl) SignUp(userSchema *schemas.CreateUserReq) (*schemas.CreateUserResp, error) {
 	existingUser, err := s.repo.GetUserByUsername(userSchema.Username)
-	if err != nil && !errors.Is(err, gorm.ErrRecordNotFound) {
-		return nil, err
-	}
 
 	if existingUser != nil {
 		return nil, errors.New("user already exists")
